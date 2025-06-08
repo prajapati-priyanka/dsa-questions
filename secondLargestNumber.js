f2dcb1131204d835cf2f8dc0e6171c04889ba636
Priyanka Prajapati <priyanka.prajapati@nuvolo.com>
2025-06-08 17:05:47 +0530

    second_largest_number

---
+++ secondLargestNumber.js
@@ -0,0 +1,29 @@
+// Write a function that returs the second largest number in an aray.
+// Input: const arr = [4,9,0,2,8,7,1]
+// Ouptut: 17
+
+
+// const arr = [4,9,0,2,8,7,1];
+const arr = [10,20,8,20,4,3,5,20,20];
+let firstLargestNum = -Infinity // smallest number in the number system
+let secondLargestNum = -Infinity
+
+const secondLargestNumber  = (arr) =>{
+    if(arr.length < 2){
+        return "Array shold have atleast 2 elements"
+    }
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > firstLargestNum){
+            secondLargestNum = firstLargestNum
+            firstLargestNum = arr[i];
+      
+        }else if((arr[i] > secondLargestNum) && (arr[i] !== firstLargestNum)){
+            secondLargestNum = arr[i];
+        }
+    }
+    return secondLargestNum;
+}
+
+
+
+console.log(secondLargestNumber(arr))
