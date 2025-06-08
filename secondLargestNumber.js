413556e3f4860f3299f78e485fabfb6abb9bbdc5
Priyanka Prajapati <priyanka.prajapati@nuvolo.com>
2025-06-08 17:09:19 +0530

    doc: add comments

--- secondLargestNumber.js
+++ secondLargestNumber.js
@@ -2,28 +2,26 @@
 // Input: const arr = [4,9,0,2,8,7,1]
 // Ouptut: 17
 
-
 // const arr = [4,9,0,2,8,7,1];
-const arr = [10,20,8,20,4,3,5,20,20];
-let firstLargestNum = -Infinity // smallest number in the number system
-let secondLargestNum = -Infinity
+const arr = [10, 20, 8, 20, 4, 3, 5, 20, 20];
+let firstLargestNum = -Infinity; // smallest number in the number system
+let secondLargestNum = -Infinity;
 
-const secondLargestNumber  = (arr) =>{
-    if(arr.length < 2){
-        return "Array shold have atleast 2 elements"
+const secondLargestNumber = (arr) => {
+  // This handles the empty array and minimum number of elements in an array corner case
+  if (arr.length < 2) {
+    return "Array shold have atleast 2 elements";
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > firstLargestNum) {
+      secondLargestNum = firstLargestNum;
+      firstLargestNum = arr[i];
+      //   This AND conditons handles duplicate numbers in an array corner case
+    } else if (arr[i] > secondLargestNum && arr[i] !== firstLargestNum) {
+      secondLargestNum = arr[i];
     }
-    for(let i=0; i<arr.length; i++){
-        if(arr[i] > firstLargestNum){
-            secondLargestNum = firstLargestNum
-            firstLargestNum = arr[i];
-      
-        }else if((arr[i] > secondLargestNum) && (arr[i] !== firstLargestNum)){
-            secondLargestNum = arr[i];
-        }
-    }
-    return secondLargestNum;
-}
+  }
+  return secondLargestNum;
+};
 
-
-
-console.log(secondLargestNumber(arr))
+console.log(secondLargestNumber(arr));
